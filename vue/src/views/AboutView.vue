72aac3c5a4b0b82fa54eea0e523d8b376383fbc1
ZJY25 <2297666392@qq.com>
2022-06-15 20:42:41 +0800

    修复vue文件夹在github中打不开问题

---
+++ vue/src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <el-button>测试{{msg}}</el-button>
+  </div>
+</template>
+<script>
+
+export default {
+  data() {
+    return {
+      msg: "zjy"
+    }
+  }
+}
+</script>
