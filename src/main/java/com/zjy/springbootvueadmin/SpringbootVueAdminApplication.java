3da7870b804197a0862a12c9791face4c49daf42
ZJY25 <2297666392@qq.com>
2022-06-15 20:01:38 +0800

    springboot项目，集成element-ui

---
+++ src/main/java/com/zjy/springbootvueadmin/SpringbootVueAdminApplication.java
@@ -0,0 +1,14 @@
+package com.zjy.springbootvueadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVueAdminApplication {
+
+    public static void main(String[] args) {
+        System.out.println("你好springboot");
+        SpringApplication.run(SpringbootVueAdminApplication.class, args);
+    }
+
+}
